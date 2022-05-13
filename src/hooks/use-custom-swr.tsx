3eeacce7a65d1c890bb1d950cdef8cc019e318b6
amir-mln <devcareer.me@gmail.com>
2022-05-13 18:06:01 +0430

    customs SWR

---
+++ src/hooks/use-custom-swr.tsx
@@ -0,0 +1,13 @@
+import fetcher from "lib/fetcher";
+import useSWR from "swr";
+
+type CustomSwrUrl = Parameters<typeof fetcher>["0"];
+type CustomSwrFetcherData = Parameters<typeof fetcher>["1"];
+
+function useCustomSWR<SwrData>(url: CustomSwrUrl, fetcherData?: CustomSwrFetcherData) {
+  const { data, error } = useSWR<SwrData>(url, (swrUrl: CustomSwrUrl) => fetcher(swrUrl, fetcherData));
+
+  return { data, error, loading: !data && !error };
+}
+
+export default useCustomSWR;
