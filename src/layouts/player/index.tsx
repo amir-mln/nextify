78386b63bcc36110b7198625007bb9d7ac523117
amir-mln <devcareer.me@gmail.com>
2022-05-13 10:47:44 +0430

    moved sidebar to components folder

--- src/layouts/player/index.tsx
+++ src/layouts/player/index.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 import { Box } from "@chakra-ui/layout";
 
-import SideBar from "./components/sidebar";
+import SideBar from "components/sidebar";
 
 function PlayerLayout({ children }: { children: ReactNode }) {
   return (
