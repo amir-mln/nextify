935a016ce00422c8e292078213db1eb2a0708c4b
amir-mln <devcareer.me@gmail.com>
2022-05-15 11:53:24 +0430

    easy peasy store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import { extendTheme, ChakraProvider } from "@chakra-ui/react";
+import { StoreProvider } from "easy-peasy";
 
 import AppLayout from "layouts/index";
 import { LAYOUT_TYPES } from "layouts/constants";
 import CHAKRA_THEME from "constants/chakra-theme";
+import store from "store/index";
 
 import "reset-css";
 
@@ -24,9 +26,12 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <ChakraProvider theme={theme}>
-      <AppLayout type={type} mainContentProps={mainContentProps}>
-        <Component {...restPageProps} />
-      </AppLayout>
+      {/* @ts-ignore : This is an issue with react 18. read: https://github.com/ctrlplusb/easy-peasy/issues/741 */}
+      <StoreProvider store={store}>
+        <AppLayout type={type} mainContentProps={mainContentProps}>
+          <Component {...restPageProps} />
+        </AppLayout>
+      </StoreProvider>
     </ChakraProvider>
   );
 }
