511c3a99a432a1607d225da222d5566c04e5426c
amir-mln <devcareer.me@gmail.com>
2022-06-03 09:25:59 +0430

    small change

--- src/lib/auth/fetcher.ts
+++ src/lib/auth/fetcher.ts
@@ -1,5 +1,14 @@
+import { User } from "@prisma/client";
 import fetcher from "lib/fetcher/index";
 
-export default function authFetcher(mode: "signin" | "signup", body: { email: string; password: string }) {
+export type AuthFetcherMods = "signin" | "signup";
+
+export type AuthFetcherBody =
+  | { email: string; password: string }
+  | { username: string; email: string; password: string };
+
+function authFetcher(mode: AuthFetcherMods, body: AuthFetcherBody): Promise<Partial<User> | Error> {
   return fetcher(`/api/auth/${mode}`, body);
 }
+
+export default authFetcher;
