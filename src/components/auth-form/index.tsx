c664f1fd679f1bc5f5a3cedc72046bcf0dd7e3dd
amir-mln <devcareer.me@gmail.com>
2022-06-03 09:43:17 +0430

    signup

--- src/components/auth-form/index.tsx
+++ src/components/auth-form/index.tsx
@@ -1,25 +1,43 @@
-import { FormEvent, FormEventHandler, useState } from "react";
-import { useSWRConfig } from "swr";
+import NextLink from "next/link";
 import { useRouter } from "next/router";
-import { Center } from "@chakra-ui/layout";
-import { Box, Input, Button } from "@chakra-ui/react";
+import { FormEvent, useState } from "react";
+import { Center, Link } from "@chakra-ui/layout";
+import { Box, Input, Button, Alert, AlertIcon } from "@chakra-ui/react";
 
 import authFetcher from "lib/auth/fetcher";
 import BrandLogo from "components/brand-logo";
 
-function AuthForm({ mode }: { mode: "signin" | "signup" }) {
+import type { AuthFetcherMods } from "lib/auth/fetcher";
+
+function AuthForm({ mode }: { mode: AuthFetcherMods }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [username, setUsername] = useState("");
+  const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
+  const inSignupMode = mode === "signup";
 
   async function handleSubmit(e: FormEvent) {
     e.preventDefault();
+
+    if (inSignupMode && password !== password2) return void setError("passwords do not match");
+
+    if (inSignupMode && !username.trim()) return void setError("invalid username");
+
     setIsLoading(true);
 
-    await authFetcher(mode, { email, password });
-    setIsLoading(false);
-    router.push("/");
+    const body = inSignupMode ? { username, password, email } : { password, email };
+
+    try {
+      await authFetcher(mode, body);
+      setIsLoading(false);
+      router.push("/");
+    } catch (error: any) {
+      setIsLoading(false);
+      setError(error.message);
+    }
   }
 
   return (
@@ -31,11 +49,24 @@ function AuthForm({ mode }: { mode: "signin" | "signup" }) {
         display="flex"
         width="33.33%"
         bgColor="gray.700"
+        marginBottom="20px"
         alignItems="center"
         flexDirection="column"
         borderRadius="sm"
         onSubmit={handleSubmit}
       >
+        {inSignupMode && (
+          <Input
+            type="text"
+            placeholder="username"
+            marginBottom="4"
+            color="gray.100"
+            borderRadius="sm"
+            bgColor="gray.800"
+            borderColor="gray.500"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        )}
         <Input
           type="email"
           placeholder="email"
@@ -56,20 +87,47 @@ function AuthForm({ mode }: { mode: "signin" | "signup" }) {
           borderColor="gray.500"
           onChange={(e) => setPassword(e.target.value)}
         />
-        <Button
-          type="submit"
-          isLoading={isLoading}
+        {inSignupMode && (
+          <Input
+            type="password"
+            placeholder="confirm password"
+            marginBottom="4"
+            color="gray.100"
+            borderRadius="sm"
+            bgColor="gray.800"
+            borderColor="gray.500"
+            onChange={(e) => setPassword2(e.target.value)}
+          />
+        )}
+        <Box
+          as="span"
+          width="100%"
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
           alignSelf="flex-start"
-          bg="green.500"
-          sx={{
-            "&:hover": {
-              bg: "green.300",
-            },
-          }}
         >
-          {mode}
-        </Button>
+          <Button
+            type="submit"
+            isLoading={isLoading}
+            bg="green.500"
+            sx={{
+              "&:hover": {
+                bg: "green.300",
+              },
+            }}
+          >
+            {mode}
+          </Button>
+          <NextLink href={`/auth/${inSignupMode ? "signin" : "signup"}`} passHref>
+            <Link>{inSignupMode ? "Already Have An Account?" : "Want To Create A New Account?"}</Link>
+          </NextLink>
+        </Box>
       </Box>
+      <Alert status="error" width="33.33%" borderRadius="sm" opacity={error ? 1 : 0} color="gray.100" bgColor="red.800">
+        <AlertIcon color="red.300" />
+        {error}
+      </Alert>
     </Center>
   );
 }
