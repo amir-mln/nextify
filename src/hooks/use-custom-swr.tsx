8696bb18d336713c1372d968096d34ac126cb2f0
amir-mln <devcareer.me@gmail.com>
2022-06-05 02:11:19 +0430

    swr options

--- src/hooks/use-custom-swr.tsx
+++ src/hooks/use-custom-swr.tsx
@@ -5,7 +5,10 @@ type CustomSwrUrl = Parameters<typeof fetcher>["0"];
 type CustomSwrFetcherData = Parameters<typeof fetcher>["1"];
 
 function useCustomSWR<SwrData>(url: CustomSwrUrl, fetcherData?: CustomSwrFetcherData) {
-  const { data, error } = useSWR<SwrData>(url, (swrUrl: CustomSwrUrl) => fetcher(swrUrl, fetcherData));
+  const { data, error } = useSWR<SwrData>(url, {
+    revalidateOnFocus: false,
+    fetcher: (swrUrl: CustomSwrUrl) => fetcher(swrUrl, fetcherData),
+  });
 
   return { data, error, loading: !data && !error };
 }
