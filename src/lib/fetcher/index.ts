36975d5500eb03012fda40c08edeccbf229ac93c
amir-mln <devcareer.me@gmail.com>
2022-06-20 18:32:07 +0430

    improvement

--- src/lib/fetcher/index.ts
+++ src/lib/fetcher/index.ts
@@ -4,22 +4,26 @@ export type ValidFetcherUrl = `/api/${string}` | `http${string}`;
 
 export type ValidFetcherBody = { [prop: string]: JsonSafeValues } | FormData;
 
-export type FetcherContentType = "application/json" | "multipart/form-data" | "application/x-www-form-urlencoded";
+export type FetcherContentType =
+  | "application/json"
+  | `multipart/form-data; boundary=${string}`
+  | "application/x-www-form-urlencoded";
 
 export default async function fetcher(url: ValidFetcherUrl, body?: ValidFetcherBody, contentType?: FetcherContentType) {
-  const res = await fetch(url, {
-    method: body ? "POST" : "GET",
-    credentials: "include",
-    headers: {
-      "Accept": "application/json, text/*",
-      "Content-Type": contentType || "application/json",
-    },
-    body: JSON.stringify(body),
-  });
+  const isFormData = body instanceof FormData;
+  const reqOptions: RequestInit = { method: "GET", credentials: "include" };
 
-  if (res.status > 399 || res.status < 200) throw new Error(res.statusText);
+  if (body) {
+    reqOptions.method = "POST";
+    reqOptions.headers = isFormData ? undefined : { "Content-Type": "application/json" };
+    reqOptions.body = isFormData ? body : JSON.stringify(body);
+  }
+
+  const res = await fetch(url, reqOptions);
 
   const data = await res.json();
+
+  if (res.status > 399 || res.status < 200) throw new Error(data.message || res.statusText);
 
   return data;
 }
