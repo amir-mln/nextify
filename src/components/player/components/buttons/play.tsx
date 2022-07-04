2c7958bb404fc36389c8579d16726c9c4d1a19c7
amir-mln <devcareer.me@gmail.com>
2022-07-04 07:22:32 +0430

    updated play button

--- src/components/player/components/buttons/play.tsx
+++ src/components/player/components/buttons/play.tsx
@@ -1,14 +1,24 @@
-import { IconButton } from "@chakra-ui/react";
+import { IconButton, TypographyProps } from "@chakra-ui/react";
 
-import { BsFillPauseCircleFill, BsFillPlayCircleFill } from "react-icons/bs";
+import { usePlayingSong } from "hooks/use-playing-songs";
 import { usePlayerDispatch, usePlayerState } from "context";
+import { BsFillPauseCircleFill, BsFillPlayCircleFill } from "react-icons/bs";
 
-function PlayButton() {
+export type PlayButtonProps = Partial<{
+  propSongName: string;
+  clickCallback: () => void;
+  fontSize: TypographyProps["fontSize"];
+}>;
+
+function PlayButton({ propSongName, fontSize, clickCallback }: PlayButtonProps) {
   const { playing } = usePlayerState();
+  const { name } = usePlayingSong() ?? {};
   const dispatch = usePlayerDispatch();
-  const buttonIcon = playing ? <BsFillPauseCircleFill /> : <BsFillPlayCircleFill />;
+  const buttonIcon =
+    playing && (name === propSongName || !propSongName) ? <BsFillPauseCircleFill /> : <BsFillPlayCircleFill />;
 
   function togglePlaying() {
+    if (clickCallback) clickCallback();
     dispatch({ type: "TOGGLE_PLAYING_STATUS" });
   }
 
@@ -17,10 +27,10 @@ function PlayButton() {
       outline="none"
       variant="link"
       color="green.500"
-      fontSize="2.5rem"
       aria-label="play"
       icon={buttonIcon}
       onClick={togglePlaying}
+      fontSize={fontSize || "2.5rem"}
     />
   );
 }
