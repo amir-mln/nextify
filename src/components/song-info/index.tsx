afd9232d8c1888163d8096029639275aeb8d1ee8
amir-mln <devcareer.me@gmail.com>
2022-07-04 07:29:26 +0430

    capitalizeString

--- src/components/song-info/index.tsx
+++ src/components/song-info/index.tsx
@@ -1,12 +1,15 @@
 import { Box, Text } from "@chakra-ui/layout";
+import { capitalizeString } from "lib/formatter";
 import { usePlayingSong } from "hooks/use-playing-songs";
 
 function SongInfo() {
   const playingSong = usePlayingSong();
+
+  if (!playingSong) return null;
   return (
     <Box>
-      <Text fontSize="large">{playingSong?.name}</Text>
-      <Text fontSize="sm">{playingSong?.artist.name}</Text>
+      <Text fontSize="large">{capitalizeString(playingSong.name)}</Text>
+      <Text fontSize="sm">{capitalizeString(playingSong.artist.name)}</Text>
     </Box>
   );
 }
--- src/lib/formatter/index.tsx
+++ src/lib/formatter/index.tsx
@@ -11,3 +11,15 @@ export const formatDate = (date: Date) => {
     day: "numeric",
   });
 };
+
+export function capitalizeString(str: string) {
+  let result: string = "";
+
+  for (const letter of str) {
+    const currentLength = result.length;
+
+    result += !currentLength || result[currentLength - 1] === " " ? letter.toUpperCase() : letter;
+  }
+
+  return result;
+}
